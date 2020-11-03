645e895e8a02d581ef5f6e55e798aceefe904838
ek2061 <jackey860517@gmail.com>
2020-11-04 01:59:43 +0800

    打包

--- build/lib/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep  5 00:41:41 2020
-
-@author: Yuchi
-"""
--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ long_description = f.read()
 setuptools.setup(
     name="hsipl_tools",
     version="0.0.1",
-    author="Yuchi",
-    author_email="jackey860517@gmail.com",
     description="Tools for Hyperspectral Signal and Image Processing Laboratory of YunTech, Taiwan",
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    author="Yuchi",
+    author_email="jackey860517@gmail.com",   
     url="https://github.com/ek2061/hsipl_tools",
-    packages=[''],
-    requires = [
+    packages=['hsipl_tools'],
+    install_requires = [
         "numpy",
         "matplotlib",
         "scipy",
         "pandas"
-    ],
-    license='MIT',
+    ],  
     python_requires='>=3.6',
 )
