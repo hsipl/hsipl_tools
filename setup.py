cba9913fd36817ec0e5cc7fa6289e2a09226aed3
ek2061 <jackey860517@gmail.com>
2021-06-25 18:07:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = f.read()
 
 setuptools.setup(
     name="hsipl_tools",
-    version="0.0.7",
+    version="0.0.8",
     description="Tools for Hyperspectral Signal and Image Processing Laboratory of YunTech, Taiwan",
     license="MIT",
     long_description=long_description,
